a77845431e8be3318c36bbf612f52d7ea8cb8ab5
Lucas Oliveira <lucas.gon.oliv@gmail.com>
2023-07-25 19:32:32 -0300

    menu com função criada

--- ex2.js
+++ ex2.js
@@ -1,15 +1,15 @@
 import PromptSync from "prompt-sync";
 const prompt = PromptSync();
 
-export function aumentoFuncionarios() {
+export function aumentoFuncionarios(){
     console.log("Uma empresa decide dar aumento de 30% aos funcionários cujo salário é inferior a 500 reais. Escreva um programa que receba o salário de um funcionário e imprima o valor do salário reajustado ou uma mensagem caso o funcionário não tenha direito a aumento.");
 
-    let salario: number = parseFloat(prompt("Informe o salário do profissional:"));
+    let salario = parseFloat (prompt("Informe o salário do profissional:"))
 
-    let salarioResult: number = (salario * 0.30) + (salario);
+    let salarioResult = (salario * 0.30)+(salario);
     if (salario < 500) {
-        console.log(`Foi acrescentado um aumento de 30% ao seu salário, seu novo salário é de ${salarioResult}`)
+    console.log(`Foi acrescentado um aumento de 30% ao seu salário, seu novo salário é de ${salarioResult}`)
     } else {
-        console.log(`Você não tem direito a aumento, seu salário é de ${salario}`)
+    console.log(`Você não tem direito a aumento, seu salário é de ${salario}`)
     }
 }
---
+++ ex3.js
@@ -0,0 +1,15 @@
+import PromptSync from "prompt-sync";
+const prompt = PromptSync();
+
+export function numInteiro(){
+    console.log("Faça um programa que receba um número inteiro do usuário e informe se este número é positivo ou negativo.");
+    let numero = parseInt(prompt("Informe o número que deseja saber se é positivo ou negativo:"))
+
+    if (numero > 0) {
+    console.log("Número positivo")
+    } else if (numero < 0) {
+    console.log("Número negativo")
+    }else {
+    console.log("Digite um número válido!")
+    }
+}
